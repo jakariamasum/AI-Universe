d9c618d76d5c49fd810d8f8694d54af7ea7befe7
jakariamasum <jmphy698@gmail.com>
2023-03-04 14:14:30 +0600

    error handeled and final push

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const fetchAllData=(limit)=>{
 
 // single items details
 const singleData=(data,limit)=>{
-     console.log( data)
     toggleLoader(true);
     if(limit && data.length>=6)
     {
@@ -22,17 +21,16 @@ const singleData=(data,limit)=>{
         document.getElementById('see-more').classList.remove('hidden');
     } 
     else {
-        document.getElementById('see-more').classList.add('hidden')
-        i=1;
+        document.getElementById('see-more').classList.add('hidden');
     }
+
      rootData=data;
     const container=document.getElementById('container');
-    container.innerText=''
+    container.innerText='';
 
     data.forEach(result=>{
-        //   console.log(fetchData)
         const div=document.createElement('div'); 
-    div.innerHTML=`
+        div.innerHTML=`
     <div class="card card-compact w-full bg-base-100 shadow-xl border-solid border-2 border-gray-500 p-5 ">
     <figure><img src="${result.image}" alt="${result.name}" class="h-[300px]" /></figure>
     <div class="card-body ">
@@ -54,80 +52,76 @@ const singleData=(data,limit)=>{
                 </div>
                 </div>
                 `
-                container.appendChild(div)
-                featuresItems(result.features,result.id)
+                container.appendChild(div);
+                featuresItems(result.features,result.id);
     })
     toggleLoader(false)                
-}
+};
 
 
 // add features list
 const featuresItems=(data,id)=>{
     //console.log(data,id)
-    const ol=document.getElementById(id)
+    const ol=document.getElementById(id);
     data.forEach(result=>{
         const li=document.createElement('li'); 
         li.innerText=result;
-        ol.appendChild(li)
+        ol.appendChild(li);
     })
 };
 
 
 // fetch modal details
 const showModal=(id)=>{
-    const url=`https://openapi.programming-hero.com/api/ai/tool/${id}`
+    const url=`https://openapi.programming-hero.com/api/ai/tool/${id}`;
     fetch(url).then(res=>res.json()).then(data=>addModalDetails(data.data))
-}
+};
 
 
 // add modal details
 const addModalDetails=(data)=>{
-    //   console.log(data)
     const modalContainer=document.getElementById('modal-container'); 
-    const {image_link,pricing,features,input_output_examples,description}=data
+    const {image_link,pricing,features,input_output_examples,description,integrations,id}=data;
     modalContainer.innerHTML=`
     <div class="lg:flex justify-between lg:p-5">
+
                 <div class="bg-red-100 border-solid border-2 border-gray-500  w-full lg:w-[55%] p-8">
                     <p class="font-semibold mb-6">${description}</p>
                     <div class="grid grid-cols-1 rounded-lg lg:grid-cols-3 gap-4 ">
-                        <p class="text-green-500 bg-green-50 text-center font-bold">${pricing?pricing[0].price:"Free of"} <br> ${pricing?pricing[0].plan:"Cost/Basic"}</p>
-                        <p class="text-yellow-500 bg-green-50 text-center" font-bold>${pricing?pricing[1].price:"Free of"} <br> ${pricing?pricing[0].plan:"Cost/Pro"}</p>
-                        <p class="text-red-500 bg-green-50 text-center font-bold">${pricing?pricing[2].price:"Free of Cost/"} <br> ${pricing?pricing[2].plan:"Enterprise"}</p>
+                        <p class="text-green-500 bg-green-50 text-center font-bold">${pricing?(pricing[0].price==='0' || pricing[0].price==='No cost')?"Free of Cost/":pricing[0].price:"Free of Cost/"} <br> ${pricing?pricing[0].plan:"Basic"}</p>
+                        <p class="text-yellow-500 bg-green-50 text-center" font-bold>${pricing?(pricing[1].price==='0' || pricing[1].price==='No cost')?"Free of Cost/":pricing[1].price:"Free of"} <br> ${pricing?pricing[1].plan:"Cost/Pro"}</p>
+                        <p class="text-red-500 bg-green-50 text-center font-bold">${pricing?(pricing[2].price==='0' || pricing[2].price==='No cost')?"Free of Cost/":pricing[2].price:"Free of Cost/"} <br> ${pricing?pricing[2].plan:"Enterprise"}</p>
                     </div>
+
                     <div class="grid grid-cols-1 mt-6 lg:grid-cols-2 gap-4">
                         <div>
                             <h1 class="text-2xl font-semibold">Features</h1>
-                        <ol id="1${data.id}" class="list-disc text-[#585858] ml-7">
-                            
-                        </ol>
+                        <ol id="1${id}" class="list-disc text-[#585858] ml-7"></ol>
                         </div>
                         <div>
                             <h1 class="text-2xl font-semibold">Integrations</h1>
-                        <ol id="2${data.id}" class="list-disc ml-7">
-                            
-                        </ol>
+                        <ol id="2${id}" class="list-disc ml-7"></ol>
                         </div>
                     </div>
                 </div>
-                <div class="border-2 border-solid border-green-500 w-full mt-5 lg:w-[45%] ml-5 mx-auto">
-                    <img src="${image_link[0]}" class="relative">
-                    ${data.accuracy.score? `<div class="badge absolute left-[880px] bottom-[400px]"">${data.accuracy.score?(data.accuracy.score)*100 +"% accuracy":''}</div>`:''}
-                    <div id="example${data.id}" class="text-center mt-6">
+
+                    <div class="border-2 border-solid border-green-500 w-full mt-5 relative lg:w-[45%] lg:ml-5">
+                    <img src="${image_link[0]}" class="relative h-[340px]">
+                    ${data.accuracy.score? `<div class="badge absolute bottom-[390px] right-[25px] bg-red-400 p-4">${data.accuracy.score?(data.accuracy.score)*100 +"% accuracy":''}</div>`:''}
+                    <div id="example${id}" class="text-center mt-6"></div>
                     </div>
-                    </div>
-                    </div>
+                </div>
                     `
-                    modalFeatures(data.features,'1'+data.id)
-                    integrationsFeatures(data.integrations,'2'+data.id)
-                    addExample(data.input_output_examples,'example'+data.id)
-                }
+                    modalFeatures(features,'1'+id)
+                    integrationsFeatures(integrations,'2'+id)
+                    addExample(input_output_examples,'example'+id)
+};
                 
                 
 // integrations Features lists
 const integrationsFeatures=(data,id)=>
 {
     const ol=document.getElementById(id);
-    //   console.log( data)
     if(data===null)
     {
         ol.innerText="No data Found";
@@ -135,35 +129,32 @@ const integrationsFeatures=(data,id)=>
     else{
         for(const singleData of data){
             const li=document.createElement('li');
-            // console.log(single)
             li.innerText=singleData; 
-            ol.appendChild(li)
+            ol.appendChild(li);
         }
     }
-}
+};
 
 // modal features lists
 const modalFeatures=(data,id)=>{
     const ol=document.getElementById(id);
      for(const single in data){
         const li=document.createElement('li');
-        // console.log(data[single].feature_name)
         li.innerText=data[single].feature_name; 
-        ol.appendChild(li)
+        ol.appendChild(li);
      }
 };
 
 
 // set example for modals
 const addExample=(data,id)=>{
-    // console.log(data,id)
     const div=document.getElementById(id);
     const p1=document.createElement('p');
     p1.classList.add('txt-2xl');
     p1.classList.add('font-semibold');
     p1.classList.add('text-[#111111]');
     const p2=document.createElement('p');
-    p2.classList.add('text-gray-500')
+    p2.classList.add('text-gray-500');
     if(data===null)
     {
         p1.innerText="Can you give any example?";
@@ -181,7 +172,7 @@ const addExample=(data,id)=>{
 };
 
 
-// add loader
+// loader functionality
 const toggleLoader=(response)=>{
     if(response)
     document.getElementById('loader').classList.remove('hidden');
@@ -192,7 +183,7 @@ const toggleLoader=(response)=>{
 // see more button's functionality
 document.getElementById('see-btn').addEventListener('click',()=>{
     fetchAllData()
-})
+});
 
 // sort btn functionality
 document.getElementById('sort-btn').addEventListener('click',function(){
@@ -207,6 +198,6 @@ document.getElementById('sort-btn').addEventListener('click',function(){
         }
         else return 0;
     };
-     singleData(rootData.sort(sorting))
+     singleData(rootData.sort(sorting));
 })
 
